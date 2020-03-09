895dfe8737411fc76fe70d998a1944d274941fbd
Jeff Milner <j.milner359@gmail.com>
2020-03-09 16:52:28 -0700

    Updated to double quotes & cleaned up

--- website.js
+++ website.js
@@ -1,118 +1,106 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const momentTZ = require('moment-timezone');
-const Sequelize = require('sequelize');
-const config = require('./config.json');
+const path = require("path");
+const momentTZ = require("moment-timezone");
+const Sequelize = require("sequelize");
+const config = require("./config.json");
 const fs = require("fs");
 
-initSite = async function() {
+const initSite = async function() {
     const sequelize = new Sequelize(config.database.data, config.database.user, config.database.pass, {
         host: config.database.host,
-        dialect: 'postgres',
-        logging: false,
-        operatorAliases: false
+        dialect: "postgres",
+        logging: false
     });
-    const changelogs = sequelize.define('changelogs', {
+    const changelogs = sequelize.define("changelogs", {
         logText: Sequelize.TEXT
     });
-    
-    let guildCount = await parseInt(fs.readFileSync(path.join(__dirname, path.sep + "/data/guildCount.txt")));
-    setInterval(async () => {
-        let guildCount = await parseInt(fs.readFileSync(path.join(__dirname, path.sep + "/data/guildCount.txt")));
-    }, 5 * 60 * 1000);
+
+    var publicDir = require("path").join(__dirname,"/public");
+    app.use(express.static(publicDir));
+
+    // Not used anymore, but could be in the future?
+    // let guildCount = await parseInt(fs.readFileSync(path.join(__dirname, path.sep + "/data/guildCount.txt")));
+    // setInterval(async () => {
+    //     guildCount = await parseInt(fs.readFileSync(path.join(__dirname, path.sep + "/data/guildCount.txt")));
+    // }, 5 * 60 * 1000);
 
     // Set the directory for the views and stuff
     app.set("views", path.join(__dirname, `..${path.sep}dashboard`));
 
     // Set the view engine to ejs
-    app.set('view engine', 'ejs');
+    app.set("view engine", "ejs");
 
     // Index page
-    app.get('/', function(req, res) {
-        res.render('pages/index', {
-            clientServers: guildCount,
-            page_name: 'index'
-        });
+    app.get("/", function(req, res) {
+        res.render("pages/index");
     });
 
     // About page
-    app.get('/about', function(req, res) {
-        res.render('pages/about', {
-            page_name: 'about'
-        });
+    app.get("/about", function(req, res) {
+        res.render("pages/about");
     });
 
     // Changelog page
-    app.get('/changelog', async function(req, res) {
-        await changelogs.findAll().then(function(logs) { 
+    app.get("/changelog", async function(req, res) {
+        await changelogs.findAll().then(function(logs) {
             const logList = [];
             const sortedLogs = logs.sort((p, c) => c.dataValues.createdAt - p.dataValues.createdAt);
             sortedLogs.forEach(log => {
-                logList.push(`<strong><font color="gray">${momentTZ.tz(log.dataValues.createdAt, 'us/pacific').format('M/D/YYYY [at] h:mm a')}</font></strong></br>${log.dataValues.logText.replace(/\n/g, '</br>')}`);
+                logList.push(`<strong><font color="gray">${momentTZ.tz(log.dataValues.createdAt, "us/pacific").format("M/D/YYYY [at] h:mm a")}</font></strong></br>${log.dataValues.logText.replace(/\n/g, "</br>")}`);
             });
 
-            res.render('pages/changelog', {
-                changelogs: logList,
-                page_name: 'changelog'
-            });
+            res.render("pages/changelog");
         });
     });
 
     // Changelog Specific page
-    app.get('/changelog/:logID', async function(req, res) {
+    app.get("/changelog/:logID", async function(req, res) {
         let id = {};
         if (!parseInt(req.params.logID)) {
             console.log("Broke trying to get log #" + req.params.logID);
         } else {
             id = {id: req.params.logID};
         }
-        await changelogs.findAll({where: id}).then(function(logs) { 
+        await changelogs.findAll({where: id}).then(function(logs) {
             const logList = [];
             const sortedLogs = logs.sort((p, c) => c.dataValues.createdAt - p.dataValues.createdAt);
             sortedLogs.forEach(log => {
-                logList.push(`<strong><font color="gray">${momentTZ.tz(log.dataValues.createdAt, 'us/pacific').format('M/D/YYYY [at] h:mm a')}</font></strong></br>${log.dataValues.logText.replace(/\n/g, '</br>')}`);
+                logList.push(`<strong><font color="gray">${momentTZ.tz(log.dataValues.createdAt, "us/pacific").format("M/D/YYYY [at] h:mm a")}</font></strong></br>${log.dataValues.logText.replace(/\n/g, "</br>")}`);
             });
 
-            res.render('pages/changelog', {
-                changelogs: logList,
-                page_name: 'changelog'
-            });
+            res.render("pages/changelog");
         });
     });
 
     // FAQs page
-    app.get('/faqs',function(req, res) {
-        res.render('pages/faqs', {
-            page_name: 'faqs'
-        });
+    app.get("/faqs",function(req, res) {
+        res.render("pages/faqs");
     });
 
-    // Commands page 
-    app.get('/commands',function(req, res) {
-        res.render('pages/commands', {
-            page_name: 'commands',
-        });
+    // Commands page
+    app.get("/commands",function(req, res) {
+        res.render("pages/commands");
     });
 
     // The link to invite the bot
-    app.get('/invite', function(req, res) {
+    app.get("/invite", function(req, res) {
         res.redirect("https://discordapp.com/oauth2/authorize/?permissions=378944&scope=bot&client_id=315739499932024834");
     });
 
     // The link to join the support server
-    app.get('/server', function(req, res) {
-        res.redirect('https://discord.gg/FfwGvhr');
+    app.get("/server", function(req, res) {
+        res.redirect("https://discord.gg/FfwGvhr");
     });
 
     app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
         console.error(err.stack);
-        res.status(500).send('Something broke!');
+        res.status(500).send("Something broke!");
     });
 
-    // The 404 Route 
-    app.use('',function(req, res) {
-        res.status(404).send('Error 404: Not Found!');
+    // The 404 Route
+    app.use("",function(req, res) {
+        res.status(404).send("Error 404: Not Found!");
     });
 
     // Turn the site on
