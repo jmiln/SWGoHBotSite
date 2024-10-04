3e2977e880b2bbd33266406c5449f1702580a7ae
jmiln <j.milner359@gmail.com>
2024-10-04 23:16:37 +0000

    feat: Switch from eslint to biome & update formatting

--- public/js/swgohbot.js
+++ public/js/swgohbot.js
@@ -1,22 +1,22 @@
 function toggleNav() {
     // Toggle the nav in mobile mode
     const menus = document.getElementById("navbar").getElementsByClassName("nav-links");
-    for (var ix = 0; ix < menus.length; ix++) {
+    for (let ix = 0; ix < menus.length; ix++) {
         menus[ix].classList.toggle("hide-mobile");
     }
     const buttons = document.getElementById("navbar").getElementsByClassName("nav-btn");
-    for (var ix = 0; ix < buttons.length; ix++) {
+    for (let ix = 0; ix < buttons.length; ix++) {
         buttons[ix].classList.toggle("hide-mobile");
     }
 }
 
-$(document).ready(function() {
-	var currentPage = location.pathname;
-	$('#navbar .nav-links').each(function(){
-		var $this = $(this);
-		// if the current path is like this link, make it active
-		if($this.attr('href') === currentPage){
-			$this.addClass('active');
-		}
-	})
-})
+$(document).ready(() => {
+    const currentPage = location.pathname;
+    $("#navbar .nav-links").each(function () {
+        const $this = $(this);
+        // if the current path is like this link, make it active
+        if ($this.attr("href") === currentPage) {
+            $this.addClass("active");
+        }
+    });
+});
--- website.js
+++ website.js
@@ -1,6 +1,6 @@
 // Native Node Imports
 // const url = require("url");
-const path = require("path");
+const path = require("node:path");
 // const fs = require("fs");
 
 // Express Session
@@ -9,8 +9,8 @@ const app = express();
 
 const config = require("./config.js");
 
-const initSite = async function() {
-    var publicDir = require("path").join(__dirname,"/public");
+const initSite = async () => {
+    const publicDir = path.join(__dirname, "/public");
     app.use(express.static(publicDir));
 
     // Not used anymore, but could be in the future? (Could probably have the bot save these to a file every hour or something?)
@@ -18,7 +18,6 @@ const initSite = async function() {
     // setInterval(async () => {
     //     guildCount = await parseInt(fs.readFileSync(path.join(__dirname, path.sep + "/data/guildCount.txt")));
     // }, 5 * 60 * 1000);
-
     // Set the directory for the views and stuff
     app.set("views", __dirname);
 
@@ -26,60 +25,62 @@ const initSite = async function() {
     app.set("view engine", "ejs");
 
     // Index page
-    app.get("/", function(req, res) {
+    app.get("/", (req, res) => {
         res.render("pages/index");
     });
 
     // About page
-    app.get("/about", function(req, res) {
+    app.get("/about", (req, res) => {
         res.render("pages/about");
     });
 
     // ToS page
-    app.get("/tos", function(req, res) {
+    app.get("/tos", (req, res) => {
         res.render("pages/tos");
     });
 
     // Privacy Policy page
-    app.get("/privacyPolicy", function(req, res) {
+    app.get("/privacyPolicy", (req, res) => {
         res.render("pages/privacyPolicy");
     });
 
     // FAQs page
-    app.get("/faqs",function(req, res) {
+    app.get("/faqs", (req, res) => {
         res.render("pages/faqs");
     });
 
     // Commands page
-    app.get("/commands",function(req, res) {
+    app.get("/commands", (req, res) => {
         res.render("pages/commands");
     });
-    app.get("/test-commands",function(req, res) {
+    app.get("/test-commands", (req, res) => {
         res.render("pages/test-commands");
     });
 
     // The link to invite the bot
-    app.get("/invite", function(req, res) {
-        res.redirect("https://discord.com/api/oauth2/authorize?client_id=315739499932024834&permissions=277025901632&scope=bot%20applications.commands");
+    app.get("/invite", (req, res) => {
+        res.redirect(
+            "https://discord.com/api/oauth2/authorize?client_id=315739499932024834&permissions=277025901632&scope=bot%20applications.commands",
+        );
     });
 
     // The link to join the support server
-    app.get("/server", function(req, res) {
+    app.get("/server", (req, res) => {
         res.redirect("https://discord.gg/FfwGvhr");
     });
 
-    app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
+    app.use((err, req, res, next) => {
         console.error(err.stack);
         res.status(500).send("Something broke!");
     });
 
     // The 404 Route
-    app.use("",function(req, res) {
+    app.use("", (req, res) => {
         res.status(404).send("Error 404: Not Found!");
     });
 
     // Turn the site on
-    app.listen(config.dashboard.port, function() {
+    app.listen(config.dashboard.port, () => {
         console.log(`Site listening on port ${config.dashboard.port}!`);
     });
 };
